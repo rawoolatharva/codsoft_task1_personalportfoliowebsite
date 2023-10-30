02255cb725e926257f09f6dc1fa0dd60320ae8e1
rawoolatharva <125192961+rawoolatharva@users.noreply.github.com>
2023-10-30 05:43:08 +0530

    Update ContactMe.jsx

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -5,8 +5,7 @@ export default function ContactMe() {
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
         <p className="text-lg">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
-          asperiores?
+         I look forward to connecting with you!
         </p>
       </div>
       <form className="contact-form-container">
